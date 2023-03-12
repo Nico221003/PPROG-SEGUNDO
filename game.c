46b4ed77b26dbdc54389012bcf263f3b956845bd
pelonsona <124621838+pelonsona@users.noreply.github.com>
2023-03-12 10:45:37 +0100

    Add files via upload

---
+++ game.c
@@ -0,0 +1,497 @@
+/**
+ * @brief It implements the game interface and all the associated calls
+ * for each command
+ *
+ * @file game.c
+ * @author Profesores PPROG
+ * @version 3.0
+ * @date 26-01-2023
+ * @copyright GNU Public License
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "game.h"
+
+/**
+   Private functions
+*/
+void game_command_exit(Game *game);
+void game_command_next(Game *game);
+void game_command_back(Game *game);
+void game_command_take(Game *game);
+void game_command_drop(Game *game);
+void game_command_left(Game *game);
+void game_command_right(Game *game);
+void game_command_attack(Game *game);
+
+/**
+   Game interface implementation
+*/
+
+STATUS game_create(Game *game)
+{
+  int i;
+
+  for (i = 0; i < MAX_SPACES; i++)
+  {
+    game->spaces[i] = NULL;
+  }
+
+  /*Creamos los objetos en el juego*/
+  game->player = player_create(PLAYER_ID);
+  game->object = object_create(OBJECT_ID);
+  game->enemy = enemy_create(ENEMY_ID);
+
+  /*game->player_location = NO_ID;
+  game->object_location = NO_ID;*/
+
+  game_set_player_location(game, 11);
+  game_set_object_location(game, 11);
+  game_set_enemy_location(game, 11);
+
+  game->last_cmd = NO_CMD;
+
+  return OK;
+}
+
+STATUS game_destroy(Game *game)
+{
+  int i = 0;
+
+  for (i = 0; i < MAX_SPACES && game->spaces[i] != NULL; i++)
+  {
+    space_destroy(game->spaces[i]);
+  }
+
+  object_destroy(game->object);
+  player_destroy(game->player);
+
+  return OK;
+}
+
+Space *game_get_space(Game *game, Id id)
+{
+  int i = 0;
+
+  if (id == NO_ID)
+  {
+    return NULL;
+  }
+
+  for (i = 0; i < MAX_SPACES && game->spaces[i] != NULL; i++)
+  {
+    if (id == space_get_id(game->spaces[i]))
+    {
+      return game->spaces[i];
+    }
+  }
+
+  return NULL;
+}
+
+STATUS game_set_player_location(Game *game, Id id)
+{
+  if (id == NO_ID)
+  {
+    return ERROR;
+  }
+
+  /*game->player_location = id;*/
+
+  player_set_location(game->player, id);
+
+  return OK;
+}
+
+STATUS game_set_enemy_location(Game *game, Id id)
+{
+  if (id == NO_ID)
+  {
+    return ERROR;
+  }
+
+  /*game->player_location = id;*/
+
+  enemy_set_location(game->enemy, id);
+
+  return OK;
+}
+
+STATUS game_set_enemy_health(Game *game, int health)
+{
+
+  /*game->player_location = id;*/
+
+  enemy_set_health(game->enemy,health);
+
+  return OK;
+}
+
+STATUS game_set_player_health(Game *game, int health)
+{
+ 
+
+  /*game->player_location = id;*/
+
+  player_set_health(game->player,health);
+
+  return OK;
+}
+
+STATUS game_set_object_location(Game *game, Id id)
+{
+
+  int aux = 0;
+
+  if (id == NO_ID)
+  {
+    return ERROR;
+  }
+
+  /*game->object_location = id;*/
+
+  object_set_location(game->object, id);
+  aux = space_set_object(game_get_space(game, id), TRUE);
+
+  if (aux == ERROR)
+  {
+    return ERROR;
+  }
+
+  return OK;
+}
+
+Id game_get_player_location(Game *game)
+{
+
+  if (!game)
+  {
+    return NO_ID;
+  }
+
+  /*return game->player_location;*/
+
+  return player_get_location(game->player);
+}
+
+int game_get_player_health(Game *game)
+{
+
+  if (!game)
+  {
+    return NO_ID;
+  }
+
+  /*return game->player_health;*/
+
+  return player_get_health(game->player);
+}
+
+Id game_get_enemy_location(Game *game)
+{
+
+  if (!game)
+  {
+    return NO_ID;
+  }
+
+  /*return game->player_location;*/
+
+  return enemy_get_location(game->enemy);
+}
+
+int game_get_enemy_health(Game *game)
+{
+
+  if (!game)
+  {
+    return NO_ID;
+  }
+
+  /*return game->player_health;*/
+
+  return enemy_get_health(game->enemy);
+}
+
+Id game_get_object_location(Game *game)
+{
+
+  if (!game)
+  {
+    return NO_ID;
+  }
+
+  /*return game->object_location;*/
+
+  return object_get_location(game->object);
+}
+
+STATUS game_update(Game *game, T_Command cmd)
+{
+  game->last_cmd = cmd;
+
+  switch (cmd)
+  {
+    case ATTACK:
+    game_command_attack(game);
+    break;
+    
+
+  case LEFT:
+    game_command_left(game);
+    break;
+
+  case RIGHT:
+    game_command_right(game);
+    break;
+
+  case EXIT:
+    game_command_exit(game);
+    break;
+
+  case NEXT:
+    game_command_next(game);
+    break;
+
+  case BACK:
+    game_command_back(game);
+    break;
+
+  case TAKE:
+    game_command_take(game);
+    break;
+
+  case DROP:
+    game_command_drop(game);
+    break;
+
+  default:
+    break;
+  }
+
+  return OK;
+}
+
+T_Command game_get_last_command(Game *game)
+{
+  return game->last_cmd;
+}
+
+void game_print_data(Game *game)
+{
+  int i = 0;
+
+  printf("\n\n-------------\n\n");
+
+  printf("=> Spaces: \n");
+  for (i = 0; i < MAX_SPACES && game->spaces[i] != NULL; i++)
+  {
+    space_print(game->spaces[i]);
+  }
+
+  /*printf("=> Object location: %d\n", (int)game->object_location);*/
+  printf("=> Object location: %d\n", (int)object_get_location(game->object));
+
+  /*printf("=> Player location: %d\n", (int)game->player_location);*/
+  printf("=> Player location: %d\n", (int)player_get_location(game->player));
+
+  
+}
+
+BOOL game_is_over(Game *game)
+{
+
+  if ((player_get_health(game->player)) == 0)
+    return ERROR;
+
+  return FALSE;
+}
+
+/**
+   Calls implementation for each action
+*/
+void game_command_attack(Game *game)
+{
+  
+  Id player_id = NO_ID;
+  Id enemy_id = NO_ID;
+  int r;
+  
+  if(!game)
+  return;
+  player_id = game_get_player_location(game);
+  if (player_id == NO_ID)
+  {
+    return;
+  }
+  enemy_id= game_get_enemy_location(game);
+  if (enemy_id == NO_ID)
+  {
+    return;
+  }
+   if(enemy_id != player_id)
+   return; 
+   if(game_get_enemy_health(game)==0)
+    return;
+  
+  r = rand () % 10;
+
+    if(r<=4)
+     game_set_player_health(game, (game_get_player_health(game)-1));
+    else
+    game_set_enemy_health(game, (game_get_enemy_health(game)-1));
+
+
+  return;
+}
+
+void game_command_left(Game *game)
+{
+  Id current_id = NO_ID;
+  Id space_id = NO_ID;
+
+  space_id = game_get_player_location(game);
+  if (space_id == NO_ID)
+  {
+    return;
+  }
+
+  current_id = space_get_west(game_get_space(game, space_id));
+  if (current_id != NO_ID)
+  {
+    game_set_player_location(game, current_id);
+  }
+
+  return;
+}
+
+void game_command_right(Game *game)
+{
+  Id current_id = NO_ID;
+  Id space_id = NO_ID;
+
+  space_id = game_get_player_location(game);
+  if (space_id == NO_ID)
+  {
+    return;
+  }
+
+  current_id = space_get_east(game_get_space(game, space_id));
+  if (current_id != NO_ID)
+  {
+    game_set_player_location(game, current_id);
+  }
+
+  return;
+}
+
+void game_command_exit(Game *game)
+{
+}
+
+void game_command_next(Game *game)
+{
+  Id current_id = NO_ID;
+  Id space_id = NO_ID;
+
+  space_id = game_get_player_location(game);
+  if (space_id == NO_ID)
+  {
+    return;
+  }
+
+  current_id = space_get_south(game_get_space(game, space_id));
+  if (current_id != NO_ID)
+  {
+    game_set_player_location(game, current_id);
+  }
+
+  return;
+}
+
+void game_command_back(Game *game)
+{
+  Id current_id = NO_ID;
+  Id space_id = NO_ID;
+
+  space_id = game_get_player_location(game);
+
+  if (NO_ID == space_id)
+  {
+    return;
+  }
+
+  current_id = space_get_north(game_get_space(game, space_id));
+  if (current_id != NO_ID)
+  {
+    game_set_player_location(game, current_id);
+  }
+
+  return;
+}
+
+void game_command_take(Game *game)
+{
+
+  Id object_id = NO_ID;
+  Id playerloc_id = NO_ID;
+  Id objectloc_id = NO_ID;
+
+  if (!game)
+  {
+    return;
+  }
+
+  /*Comprobar que player esté en el mismo hab que el objeto*/
+  playerloc_id = game_get_player_location(game);
+  objectloc_id = game_get_object_location(game);
+
+  if (playerloc_id != objectloc_id)
+  {
+    return;
+  }
+
+  /*Meter id de objeto en player->object y poner object->location a NO_ID*/
+  object_id = object_get_id(game->object);
+  object_set_location(game->object, NO_ID);
+
+  player_set_object(game->player, object_id);
+
+  return;
+}
+
+void game_command_drop(Game *game)
+{
+
+  Id objectloc_id = NO_ID;
+  Id objectplayer_id = NO_ID;
+
+  if (!game)
+  {
+    return;
+  }
+
+  /*Comprobación de que el objeto no esté en el mapa*/
+  objectloc_id = game_get_object_location(game);
+  if (objectloc_id != NO_ID)
+  {
+    return;
+  }
+
+  /*Comprobación de que el player tenga objeto*/
+  objectplayer_id = player_get_object(game->player);
+  if (objectplayer_id == NO_ID)
+  {
+    return;
+  }
+
+  /*Ponemos el objeto del player a NO_ID*/
+  /*Seteamos el objeto donde está el player*/
+  player_set_object(game->player, NO_ID);
+
+  object_set_location(game->object, player_get_location(game->player));
+
+  return;
+}
---
+++ game.h
@@ -0,0 +1,194 @@
+/**
+ * @brief It defines the game interface
+ *
+ * @file game.h
+ * @author Profesores PPROG
+ * @version 2.0
+ * @date 29-11-2021
+ * @copyright GNU Public License
+ */
+
+#ifndef GAME_H
+#define GAME_H
+
+#include "command.h"
+#include "space.h"
+#include "object.h"
+#include "player.h"
+#include "enemy.h"
+#include "types.h"
+
+typedef struct _Game {
+  Object *object;
+  Player *player;
+  Enemy *enemy;
+  Space *spaces[MAX_SPACES];
+  T_Command last_cmd;
+} Game;
+
+
+/**
+ * @author Profesores PPROG
+ * @brief Creates the game and its elements (calls other create functs) + sets locations
+ * 
+ * @param game pointer to the game struct
+ * @return OK or ERROR in case of error
+ */
+STATUS game_create(Game *game);
+
+
+/**
+ * @author Profesores PPROG
+ * @brief Reads the command introduced by the user
+ * 
+ * @param game pointer to the game struct
+ * @param cmd the command introduced on the game
+ * @return OK or ERROR in case of error
+ */
+STATUS game_update(Game *game, T_Command cmd);
+
+/**
+ * @author Alejandra Palma
+ * @brief Reads the command introduced by the user
+ * 
+ * @param game pointer to the game struct
+ * @param cmd the command introduced on the game
+ * @return OK or ERROR in case of error
+ */
+STATUS game_set_enemy_health(Game *game, int health);
+
+/**
+ * @author Alejandra Palma
+ * @brief Reads the command introduced by the user
+ * 
+ * @param game pointer to the game struct
+ * @param cmd the command introduced on the game
+ * @return OK or ERROR in case of error
+ */
+STATUS game_set_player_health(Game *game, int health);
+
+
+/**
+ * @author Profesores PPROG
+ * @brief Destroys the game and its elements, freeing memory
+ * 
+ * @param game pointer to the game struct
+ * @return OK or ERROR in case of error
+ */
+STATUS game_destroy(Game *game);
+
+
+/**
+ * @author Profesores PPROG
+ * @brief sets the location of the player in the game
+ * 
+ * @param game pointer to the game struct
+ * @param id id of the space
+ * @return OK or ERROR in case of error
+ */
+STATUS game_set_player_location(Game *game, Id id);
+
+/**
+ * @author Profesores PPROG
+ * @brief sets the location of the enemy in the game
+ * 
+ * @param game pointer to the game struct
+ * @param id id of the space
+ * @return OK or ERROR in case of error
+ */
+STATUS game_set_enemy_location(Game *game, Id id);
+
+
+/**
+ * @author Profesores PPROG
+ * @brief sets the location of the object in the game
+ * 
+ * @param game pointer to the game struct
+ * @param id id of the space
+ * @return OK or ERROR in case of error
+ */
+STATUS game_set_object_location(Game *game, Id id);
+
+
+/**
+ * @author Profesores PPROG
+ * @brief Determines if the game is over or not
+ * 
+ * @param game pointer to the game struct
+ * @return TRUE or FALSE
+ */
+BOOL game_is_over(Game *game);
+
+
+/**
+ * @author Profesores PPROG
+ * @brief Prints the data of the game
+ * 
+ * @param game pointer to the game struct
+ */
+void game_print_data(Game *game);
+
+
+/**
+ * @author Profesores PPROG
+ * @brief get the space of the game
+ */
+Space *game_get_space(Game *game, Id id);
+
+
+/**
+ * @author Profesores PPROG
+ * @brief Gets the location of the player in the game
+ * 
+ * @param game pointer to the game struct
+ * @return id of the location of the player
+ */
+Id game_get_player_location(Game *game);
+
+/**
+ * @author Profesores PPROG
+ * @brief Gets the location of the player in the game
+ * 
+ * @param game pointer to the game struct
+ * @return id of the location of the player
+ */
+int game_get_player_health(Game *game);
+
+/**
+ * @author Profesores PPROG
+ * @brief Gets the location of the player in the game
+ * 
+ * @param game pointer to the game struct
+ * @return id of the location of the player
+ */
+Id game_get_enemy_location(Game *game);
+
+/**
+ * @author Profesores PPROG
+ * @brief Gets the location of the player in the game
+ * 
+ * @param game pointer to the game struct
+ * @return id of the location of the player
+ */
+int game_get_enemy_health(Game *game);
+
+
+/**
+ * @author Profesores PPROG
+ * @brief Gets the location of the object in the game
+ * 
+ * @param game pointer to the game struct
+ * @return id of the location of the object
+ */
+Id game_get_object_location(Game *game);
+
+
+/**
+ * @author Profesores PPROG
+ * @brief Self explanatory (gets last command introduced by the user)
+ * 
+ * @param game pointer to the game Struct
+ * @return last_cmd
+ */
+T_Command game_get_last_command(Game *game);
+#endif
